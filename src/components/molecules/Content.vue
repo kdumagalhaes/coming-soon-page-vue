cd64d774d7fceb3a1acbaca3e083e3288c6d7436
kdumagalhaes <ce.magalhaespereira@gmail.com>
2021-04-09 19:08:31 -0300

    finished components reorganization

---
+++ src/components/molecules/Content.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="content">
+        <header class="content__logo">
+            <Logo />
+        </header>
+        <div class="content__text">
+            <HeroTitle text="We're" strong="coming soon" />
+            <HeroParagraph
+                text="Hello fellow shoppers! We're currently building our new fashion store. Add your email below to stay up-to-date with announcements and our launch deals."
+            />
+            <NewsForm />
+        </div>
+    </div>
+</template>
+
+<script>
+//atoms
+import { HeroTitle, HeroParagraph, NewsForm, Logo } from '@/components/atoms'
+
+export default {
+    components: {
+        HeroTitle,
+        HeroParagraph,
+        NewsForm,
+        Logo,
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.content {
+    width: 100%;
+    max-width: 450px;
+    margin: 40px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    .content__logo {
+        margin-bottom: 100px;
+    }
+
+    .content__text {
+    }
+}
+</style>
---
+++ src/components/templates/MainContainer.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="main-container">
+        <div class="main-container__content">
+            <Content />
+        </div>
+        <div class="main-container__picture">
+            <ModelPicture />
+        </div>
+    </div>
+</template>
+
+<script>
+// molecules
+import { Content } from '@/components/molecules'
+
+// atoms
+import { ModelPicture } from '@/components/atoms'
+
+export default {
+    components: {
+        Content,
+        ModelPicture,
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.main-container {
+    display: flex;
+    justify-content: space-between;
+    background-image: url('~@/assets/img/bg-pattern-desktop.svg');
+
+    .main-container__content {
+        display: flex;
+        align-items: center;
+        margin: 0 auto;
+    }
+}
+</style>
